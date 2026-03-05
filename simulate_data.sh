02eebecbe54b360e142f6c58284d1d12dbfdb1a4
Madeleine Pittigher <77864767+maddie1231@users.noreply.github.com>
2026-03-04 20:27:44 -0800

    Add comments to simulate_data.sh for clarity
    Added comments to explain the purpose of the script and its functionality.

--- simulate_data.sh
+++ simulate_data.sh
@@ -1,1 +1,5 @@
+#Base line code to simulate GWAS dataset from 1000 genotypes dataset. Then we can use 1000 genomes
+#genotypes to run gcta LMM based heritability estimation.
 
+#We will run this multiple times with different parameters to simulate different GWAS sumstats.
+gcta64 --bfile test --simu-qt --simu-hsq 0.5 --simu-rep 3 --out test
