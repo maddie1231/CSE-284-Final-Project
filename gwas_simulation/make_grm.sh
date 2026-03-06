6fbf7a08a22f482f3641ad7e2fc51c952b17b3c1
Madeleine Pittigher <77864767+maddie1231@users.noreply.github.com>
2026-03-06 11:00:42 -0800

    Add command to generate GRM in make_grm.sh

--- gwas_simulation/make_grm.sh
+++ gwas_simulation/make_grm.sh
@@ -1,1 +1,4 @@
-
+/nfs/lab/tscc/mpittigher/random/gcta-1.95.1-linux-x86_64/squashfs-root/usr/bin/gcta64 \
+  --bfile /nfs/lab/tscc/ref/LDSC/1000G_EUR_Phase3_plink/1000G.EUR.QC.6 \
+  --make-grm \
+  --out chr6_grm
