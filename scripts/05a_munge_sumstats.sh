23861dc6eaf236b82978d7f5b7ccb7311c3167b8
Rosan Wang <rosanwang@yahoo.com>
2026-03-13 14:07:40 -0700

    updates

--- scripts/05a_munge_sumstats.sh
+++ scripts/05a_munge_sumstats.sh
@@ -7,13 +7,23 @@ tar -jxvf eur_w_ld_chr.tar.bz2
 ##### step 1 ###
 # input: simulated gwas summary statistics with varying h2 
 # output: summary statistics in ldsc format
+cd /Users/rosanwang/ldsc
+conda activate ldsc 
 
 indir=/Users/rosanwang/Documents/school/ucsd/year\ 2/CSE\ 284/project/gwas_simulation
 outdir=/Users/rosanwang/Documents/school/ucsd/year\ 2/CSE\ 284/project/ldsc/sumstats
 num=489
 
-for f in ${indir}/T1D/T1D*.mlma
-do 
+# Define the array
+files=(
+  ${indir}/T1D/T1D_sim_gwas_HLA.mlma
+  ${indir}/Schizophrenia/Schizophrenia_sim_gwas_HLA.mlma
+  ${indir}/MDD/MDD_sim_gwas_LINC.mlma
+  ${indir}/Autism/Autism_sim_gwas_MACROD2.mlma
+  ${indir}/Anorexia/Anorexia_sim_gwas_FOXP1.mlma
+)
+
+for f in "${files[@]}"; do
     name=$(basename "$f" .mlma)
     echo "${name}"
     ./munge_sumstats.py \
@@ -30,6 +40,14 @@ done
 outdir=/Users/rosanwang/Documents/school/ucsd/year\ 2/CSE\ 284/project/ldsc/sumstats
 ref=/Users/rosanwang/Documents/school/ucsd/year\ 2/CSE\ 284/project/ldsc/ref/baselineLD_v2.3/baselineLD.
 weights=/Users/rosanwang/Documents/school/ucsd/year\ 2/CSE\ 284/project/ldsc/ref/GRCh38/weights/weights.hm3_noMHC.
+
+files=(
+  ${outdir}/T1D_sim_gwas_HLA.sumstats.gz
+  ${outdir}/Schizophrenia_sim_gwas_HLA.sumstats.gz
+  ${outdir}/MDD_sim_gwas_LINC.sumstats.gz
+  ${outdir}/Autism_sim_gwas_MACROD2.sumstats.gz
+  ${outdir}/Anorexia_sim_gwas_FOXP1.sumstats.gz
+)
 
 for file in ${outdir}/*.sumstats.gz; do
     name=$(basename "$file" .sumstats.gz)
