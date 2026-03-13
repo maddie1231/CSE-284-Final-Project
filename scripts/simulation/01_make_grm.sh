177e446e532bfaba64cf9a5469de415975e52b54
Madeleine Pittigher <77864767+maddie1231@users.noreply.github.com>
2026-03-13 11:10:35 -0700

    Update script comments for path instructions

--- scripts/simulation/01_make_grm.sh
+++ scripts/simulation/01_make_grm.sh
@@ -1,5 +1,4 @@
-#Script to call GCTA to make genetic relationship matrix for 1000G EUR chromosomes needed for the traits we are studying. (Paths point to files on lab cluster but references are included in ref folder on github)
-
+#Script to call GCTA to make genetic relationship matrix for 1000G EUR chromosomes needed for the traits we are studying. (Paths point to files on lab cluster, change to your path from downloaded 1000G data)
 
 /nfs/lab/tscc/mpittigher/random/gcta-1.95.1-linux-x86_64/squashfs-root/usr/bin/gcta64 \
   --bfile /nfs/lab/tscc/ref/LDSC/1000G_EUR_Phase3_plink/1000G.EUR.QC.6 \
