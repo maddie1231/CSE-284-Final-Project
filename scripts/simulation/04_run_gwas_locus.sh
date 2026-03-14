26e9cf427db99fd3f72cde931528ce5333edf4e9
Madeleine Pittigher <77864767+maddie1231@users.noreply.github.com>
2026-03-13 22:26:07 -0700

    Update paths and output locations in GWAS script

--- scripts/simulation/04_run_gwas_locus.sh
+++ scripts/simulation/04_run_gwas_locus.sh
@@ -1,31 +1,31 @@
 #Script to run a mlma GWAS on GCTA simulated GWAS data for each trait.
 
-/nfs/lab/tscc/mpittigher/random/gcta-1.95.1-linux-x86_64/squashfs-root/usr/bin/gcta64 \
-  --bfile /nfs/lab/tscc/mpittigher/random/new_plinks/chr6_HLA/chr6_HLA \
-  --pheno T1D_results/T1D_sim_HLAregion.phen \
+/usr/bin/time -v gcta64 \
+  --bfile inputs/filtered_plinks/chr6_HLA \
+  --pheno inputs/gwas_simulation/T1D/T1D_sim_HLAregion.phen \
   --mlma \
-  --out GWAS_results/T1D_sim_gwas_HLA
+  --out inputs/gwas_simulation/T1D/T1D_sim_gwas_HLA
   
-  /nfs/lab/tscc/mpittigher/random/gcta-1.95.1-linux-x86_64/squashfs-root/usr/bin/gcta64 \
-  --bfile /nfs/lab/tscc/mpittigher/random/new_plinks/chr3_FOXP1 \
-  --pheno Anorexia_results/Anorexia_sim_FOXP1.phen \
+  /usr/bin/time -v gcta64 \
+  --bfile inputs/filtered_plinks/chr3_FOXP1 \
+  --pheno inputs/gwas_simulation/Anorexia/Anorexia_sim_FOXP1.phen \
   --mlma \
-  --out GWAS_results/Anorexia_sim_gwas_FOXP1
+  --out inputs/gwas_simulation/Anorexia/Anorexia_sim_gwas_FOXP1
   
-  /nfs/lab/tscc/mpittigher/random/gcta-1.95.1-linux-x86_64/squashfs-root/usr/bin/gcta64 \
-  --bfile /nfs/lab/tscc/mpittigher/random/new_plinks/chr6_HLA/chr6_HLA \
-  --pheno Schizophrenia_results/Schizophrenia_sim_HLA.phen \
+  /usr/bin/time -v gcta64 \
+  --bfile inputs/filtered_plinks/chr6_HLA \
+  --pheno inputs/gwas_simulation/Schizophrenia/Schizophrenia_sim_HLA.phen \
   --mlma \
-  --out GWAS_results/Schizophrenia_sim_gwas_HLA
+  --out inputs/gwas_simulation/Schizophrenia/Schizophrenia_sim_gwas_HLA
   
-  /nfs/lab/tscc/mpittigher/random/gcta-1.95.1-linux-x86_64/squashfs-root/usr/bin/gcta64 \
-  --bfile /nfs/lab/tscc/mpittigher/random/new_plinks/chr13_LINC \
-  --pheno MDD_results/MDD_sim_LINC.phen \
+  /usr/bin/time -v gcta64 \
+  --bfile inputs/filtered_plinks/chr13_LINC \
+  --pheno inputs/gwas_simulation/MDD/MDD_sim_LINC.phen \
   --mlma \
-  --out GWAS_results/MDD_sim_gwas_LINC
+  --out inputs/gwas_simulation/MDD/MDD_sim_gwas_LINC
   
-  /nfs/lab/tscc/mpittigher/random/gcta-1.95.1-linux-x86_64/squashfs-root/usr/bin/gcta64 \
-  --bfile /nfs/lab/tscc/mpittigher/random/new_plinks/chr20_MACROD2 \
-  --pheno Autism_results/Autism_sim_MACROD2.phen \
+  /usr/bin/time -v gcta64 \
+  --bfile inputs/filtered_plinks/chr20_MACROD2 \
+  --pheno inputs/gwas_simulation/Autism/Autism_sim_MACROD2.phen \
   --mlma \
-  --out GWAS_results/Autism_sim_gwas_MACROD2
+  --out inputs/gwas_simulation/MDD/Autism_sim_gwas_MACROD2
