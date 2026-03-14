e9c2dbc29ec016a8b92ce90a896f812cca0bc7c5
Madeleine Pittigher <77864767+maddie1231@users.noreply.github.com>
2026-03-13 22:05:20 -0700

    Update paths and output directories in GRM script

--- scripts/simulation/01_make_grm_locus.sh
+++ scripts/simulation/01_make_grm_locus.sh
@@ -1,21 +1,21 @@
-#Script to call GCTA to make genetic relationship matrix for 1000G EUR specific loci needed for the traits we are studying. (Paths point to files on lab cluster, change to your path to downloaded data)
+#Script to call GCTA to make genetic relationship matrix for 1000G EUR specific loci needed for the traits we are studying.
 
-/nfs/lab/tscc/mpittigher/random/gcta-1.95.1-linux-x86_64/squashfs-root/usr/bin/gcta64 \
-  --bfile /nfs/lab/tscc/mpittigher/random/new_plinks/chr6_HLA/chr6_HLA \
+/usr/bin/time -v gcta64 \
+  --bfile inputs/filtered_plinks/chr6_HLA \
   --make-grm \
-  --out chr6_HLA_grm
+  --out inputs/grms/chr6_HLA_grm
   
-  /nfs/lab/tscc/mpittigher/random/gcta-1.95.1-linux-x86_64/squashfs-root/usr/bin/gcta64 \
-  --bfile /nfs/lab/tscc/mpittigher/random/new_plinks/chr3_FOXP1 \
+  /usr/bin/time -v gcta64 \
+  --bfile inputs/filtered_plinks/chr3_FOXP1 \
   --make-grm \
-  --out chr3_FOXP1_grm
+  --out inputs/grms/chr3_FOXP1_grm
   
-  /nfs/lab/tscc/mpittigher/random/gcta-1.95.1-linux-x86_64/squashfs-root/usr/bin/gcta64 \
-  --bfile /nfs/lab/tscc/mpittigher/random/new_plinks/chr13_LINC \
+  /usr/bin/time -v gcta64 \
+  --bfile inputs/filtered_plinks/chr13_LINC \
   --make-grm \
-  --out chr13_LINC_grm
+  --out inputs/grms/chr13_LINC_grm
   
-  /nfs/lab/tscc/mpittigher/random/gcta-1.95.1-linux-x86_64/squashfs-root/usr/bin/gcta64 \
-  --bfile /nfs/lab/tscc/mpittigher/random/new_plinks/chr20_MACROD2 \
+  /usr/bin/time -v gcta64 \
+  --bfile inputs/filtered_plinks/chr20_MACROD2 \
   --make-grm \
-  --out chr20_MACROD2_grm
+  --out cinputs/grms/hr20_MACROD2_grm
