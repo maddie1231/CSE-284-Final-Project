bd1a784c2d57da50de9723f3cdab616eda3456c6
Madeleine Pittigher <77864767+maddie1231@users.noreply.github.com>
2026-03-13 22:02:53 -0700

    Change output directory for GRM files

--- scripts/simulation/01_make_grm.sh
+++ scripts/simulation/01_make_grm.sh
@@ -3,19 +3,19 @@
 /usr/bin/time -v gcta64 \
   --bfile /path/to/your/downloaded/1000G/chr6 \
   --make-grm \
-  --out chr6_grm
+  --out grms/chr6_grm
   
   /usr/bin/time -v gcta64 \
   --bfile /path/to/your/downloaded/1000G/chr3 \
   --make-grm \
-  --out chr3_grm
+  --out grms/chr3_grm
   
   /usr/bin/time -v gcta64 \
   --bfile /path/to/your/downloaded/1000G/chr13 \
   --make-grm \
-  --out chr13_grm
+  --out grms/chr13_grm
   
   /usr/bin/time -v gcta64 \
   --bfile /path/to/your/downloaded/1000G/chr20 \
   --make-grm \
-  --out chr20_grm
+  --out grms/chr20_grm
